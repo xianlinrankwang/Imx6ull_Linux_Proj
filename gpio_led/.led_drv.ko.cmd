47f650cd7e4ae47eb365766ade80815709729ffc
xianlinrankwang <824707278@qq.com>
2023-03-27 14:07:04 +0800

    删除编译后产生的文件，over

--- gpio_led/.led_drv.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/alientek/alpha/alientek-zzk/demo/linux_learn/gpio_led/led_drv.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/alientek/alpha/alientek-zzk/demo/linux_learn/gpio_led/led_drv.ko /home/alientek/alpha/alientek-zzk/demo/linux_learn/gpio_led/led_drv.o /home/alientek/alpha/alientek-zzk/demo/linux_learn/gpio_led/led_drv.mod.o
--- gpio_led/.tmp_versions/led_drv.mod
+++
@@ -1,2 +0,0 @@
-/home/alientek/alpha/alientek-zzk/demo/linux_learn/gpio_led/led_drv.ko
-/home/alientek/alpha/alientek-zzk/demo/linux_learn/gpio_led/led_drv.o
